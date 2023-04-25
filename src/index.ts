3f42fb2e4ba4ef66ab9be1694a87b8727795ecdd
Steven Thompson <steven@thompsonsj.com>
2023-04-25 14:54:02 +0100

    fix(index): exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export { crowdInSync } from './plugin'
+import { crowdinClient } from './api'
+import { mockCrowdinClient } from './api/mock/crowdin-client'
+import {
+  updateTranslation
+} from './api/payload'
+
+import { crowdInSync } from './plugin'
+
+export {
+  crowdInSync,
+  crowdinClient,
+  mockCrowdinClient,
+  updateTranslation
+}
