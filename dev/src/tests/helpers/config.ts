0c6ee1a91419598c028c641073858ea60ce8ed8b
Steven Thompson <44806974+thompsonsj@users.noreply.github.com>
2023-06-30 11:28:46 +0100

    build(dev): integration test setup (#37)

---
+++ dev/src/tests/helpers/config.ts
@@ -0,0 +1,52 @@
+import payload from 'payload';
+import swcRegister from '@swc/register';
+import { v4 as uuid } from 'uuid';
+import type { InitOptions } from 'payload/dist/config/types';
+import path from 'path';
+import express from 'express';
+
+type Options = {
+  __dirname: string;
+  init?: Partial<InitOptions>;
+};
+
+export async function initPayloadTest(options: Options): Promise<{ serverURL: string }> {
+  const initOptions = {
+    local: true,
+    secret: uuid(),
+    mongoURL: `mongodb://localhost/${uuid()}`,
+    ...(options.init || {}),
+  };
+
+  process.env.NODE_ENV = 'test';
+  process.env.PAYLOAD_CONFIG_PATH = path.resolve(options.__dirname, './../payload.config.ts');
+
+  const port = process.env.PORT || 3000;
+
+  if (!initOptions?.local) {
+    initOptions.express = express();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore - bad @swc/register types
+  swcRegister({
+    sourceMaps: 'inline',
+    jsc: {
+      parser: {
+        syntax: 'typescript',
+        tsx: true,
+      },
+    },
+    module: {
+      type: 'commonjs',
+    },
+  });
+
+  await payload.init(initOptions);
+
+  if (initOptions.express) {
+    initOptions.express.listen(port);
+  }
+
+  return { serverURL: `http://localhost:${port}` };
+}
--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,10 @@ import type {Config} from 'jest';
 
 const config: Config = {
   verbose: true,
-  modulePathIgnorePatterns: ["dist"]
+  modulePathIgnorePatterns: ["dist"],
+  testEnvironment: 'node',
+  // globalSetup: '<rootDir>/dev/src/tests/globalSetup.ts',
+  roots: ['<rootDir>/src/', '<rootDir>/dev/'],
 };
 
 export default config;
