e867734460e8987831d481f9504fc0f3050cca60
Steven Thompson <steven@thompsonsj.com>
2023-10-08 15:21:44 +0100

    test(jest.config.ts): try transforms again

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,14 @@ import type { Config } from "jest";
 
 const config: Config = {
   verbose: true,
-  modulePathIgnorePatterns: ["dev/dist"],
+  modulePathIgnorePatterns: ["dist"],
   testTimeout: 90000,
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    "node_modules/get-port/.+\\.(j|t)sx?$": "ts-jest"
   },
+  "transformIgnorePatterns": [
+    "node_modules/(?!get-port/.*)"
+  ],
   testEnvironment: "node",
   // globalSetup: '<rootDir>/dev/src/tests/globalSetup.ts',
   roots: ["<rootDir>/src/", "<rootDir>/dev/"],
