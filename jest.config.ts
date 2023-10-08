e1518ea9dd71966b02b2feef58509ffc4f84c68e
Steven Thompson <steven@thompsonsj.com>
2023-10-08 18:11:53 +0100

    test(babel.config): try removing like in payload repo

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  preset: "@shelf/jest-mongodb",
   verbose: true,
   modulePathIgnorePatterns: ["dist"],
   testTimeout: 90000,
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
   testEnvironment: "node",
   // globalSetup: '<rootDir>/dev/src/tests/globalSetup.ts',
   roots: ["<rootDir>/src/", "<rootDir>/dev/"],
