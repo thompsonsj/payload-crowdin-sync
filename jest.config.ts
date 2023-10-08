9ae6aec785539e86f5be74f14dc1f4eb3e75e95c
Steven Thompson <steven@thompsonsj.com>
2023-10-08 15:31:18 +0100

    test(jest.config.ts): try a different preset

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,10 @@
 import type { Config } from "jest";
 
 const config: Config = {
+  preset: 'ts-jest/presets/js-with-ts',
   verbose: true,
   modulePathIgnorePatterns: ["dist"],
   testTimeout: 90000,
-  transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
-    "node_modules/get-port/.+\\.(j|t)sx?$": "@swc/jest"
-  },
-  "transformIgnorePatterns": [
-    "node_modules/(?!get-port/.*)"
-  ],
   testEnvironment: "node",
   // globalSetup: '<rootDir>/dev/src/tests/globalSetup.ts',
   roots: ["<rootDir>/src/", "<rootDir>/dev/"],
