fbf34a22d18eb2c453e60b5efad4fb3b9151e17c
Steven Thompson <steven@thompsonsj.com>
2023-10-08 14:55:47 +0100

    test(jest.config.ts): try emulating config from payload

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,9 @@ import type { Config } from "jest";
 const config: Config = {
   verbose: true,
   modulePathIgnorePatterns: ["dist"],
-  moduleDirectories: [
-    "node_modules"
-  ],
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
   testEnvironment: "node",
   // globalSetup: '<rootDir>/dev/src/tests/globalSetup.ts',
   roots: ["<rootDir>/src/", "<rootDir>/dev/"],
