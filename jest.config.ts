e6838b8afd5c52c74c445d857590a32e51086e6c
Steven Thompson <steven@thompsonsj.com>
2023-10-08 14:33:27 +0100

    fix(jest.config.ts): add "transformIgnorePatterns"

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "jest";
 const config: Config = {
   verbose: true,
   modulePathIgnorePatterns: ["dist"],
+  transformIgnorePatterns: [
+    "node_modules/(?!get-port)"
+  ],
   testEnvironment: "node",
   // globalSetup: '<rootDir>/dev/src/tests/globalSetup.ts',
   roots: ["<rootDir>/src/", "<rootDir>/dev/"],
