121d6c4ed5f762a4bb3123336e68683557ff5f26
Steven Thompson <44806974+thompsonsj@users.noreply.github.com>
2026-04-16 11:26:04 +0100

    fix(tests): mongodb collation setting to accommodate our locales

--- dev/src/tests/generateDatabaseAdapter.js
+++ dev/src/tests/generateDatabaseAdapter.js
@@ -15,9 +15,6 @@ export const allDatabaseAdapters = {
       process.env.MONGODB_MEMORY_SERVER_URI ||
       process.env.DATABASE_URI ||
       'mongodb://127.0.0.1/payloadtests',
-    collation: {
-      strength: 1,
-    },
   })`,
   postgres: `
   import { postgresAdapter } from '@payloadcms/db-postgres'
